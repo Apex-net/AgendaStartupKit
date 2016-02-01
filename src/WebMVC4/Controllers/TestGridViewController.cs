df4b6404647f551255341fdb4e0c8b70830d2b51
Ali Servet Donmez <asd@pittle.org>
2016-02-01 17:51:05 +0100

    Aggiunto DX grid view d'esempio

---
+++ src/WebMVC4/Controllers/TestGridViewController.cs
@@ -0,0 +1,25 @@
+﻿namespace WebMVC4.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Agenda.Model;
+    using WebMVC4.Models;
+
+    public class TestGridViewController : Controller
+    {
+        public ActionResult Show()
+        {
+            var model = new GridControlModel("testGridView") { ViewModel = GetUtenti() };
+
+            return this.PartialView(model);
+        }
+
+        private static IEnumerable<object> GetUtenti()
+        {
+            using (var context = new AgendaContext())
+            {
+                return context.Set<Utenti>();
+            }
+        }
+    }
+}
